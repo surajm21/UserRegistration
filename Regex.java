9a6b5057849b884785ec47f7b921adf9eb74ca73
suraj <surajmore2143@gmail.com>
2022-06-21 13:09:07 +0530

    Added to validate mobile number in regex feature

--- Regex.java
+++ Regex.java
@@ -5,15 +5,15 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Regex {
-    public static boolean isValidUsername(String email) {
-        String regex = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
+    public static boolean isValidUsername(String mobileNo) {
+        String regex = "^[0-9]{2}\\s[1-9]{1}[0-9]{9}$";
         Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(email);
+        Matcher matcher = pattern.matcher(mobileNo);
         return matcher.matches();
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter your email : ");
+        System.out.println("Please enter your mobile number : ");
         String str = sc.nextLine();
         System.out.println(isValidUsername(str));
     }
