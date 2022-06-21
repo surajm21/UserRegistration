21787ba412340563cfa9433e29316e420cb1b47f
suraj <surajmore2143@gmail.com>
2022-06-21 16:54:45 +0530

    Added to password should have at least one upper case in regex feature

--- Regex.java
+++ Regex.java
@@ -5,15 +5,15 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Regex {
-    public static boolean isValidUsername(String password) {
-        String regex = "^[A-Za-z]{8,}$";
+    public static boolean isValidUsername(String upperCase) {
+        String regex = "^(?=.*[A-Z])[A-Za-z]{8,}$";
         Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(password);
+        Matcher matcher = pattern.matcher(upperCase);
         return matcher.matches();
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter your password : ");
+        System.out.println("Please enter your password at minimum one letter in uppercase : ");
         String str = sc.nextLine();
         System.out.println(isValidUsername(str));
     }
